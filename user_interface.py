2746d61aa21cacd7d00f77a678181040021124e4
cwyd0822 <cwyd0822@qq.com>
2019-12-30 17:21:28 +0800

    添加广播

--- user_interface.py
+++ user_interface.py
@@ -151,6 +151,12 @@ class BluetoothDevice():
         self.scontrol.listen(1)  # Limit of 1 connection
         self.sinterrupt.listen(1)
 
+        bluetooth.advertise_service(self.scontrol, "SampleServer", service_id=self.UUID,
+                                    service_classes=[self.UUID, bluetooth.SERIAL_PORT_CLASS],
+                                    profiles=[bluetooth.SERIAL_PORT_PROFILE],
+                                    # protocols=[bluetooth.OBEX_UUID]
+                                    )
+
         self.ccontrol, cinfo = self.scontrol.accept()
         print("Got a connection on the control channel from " + cinfo[0])
 
